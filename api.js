21c1347d6dfc0e4cc707599bc5d414653e2b7812
Nathan Marques de Aguiar Brandão <nathanbrandao1@gmail.com>
2022-10-26 00:03:07 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'api_key=b92d93c813e08c4c22bb45e911908d1d';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
